0fea6a1a23270ce6ed6b06f94c305c12a0b03002
Ravindu Rasanjana <145350557+Ravindu-Rasanjana@users.noreply.github.com>
2023-11-11 12:07:29 +0530

    Add files via upload

---
+++ IF05test.py
@@ -0,0 +1,8 @@
+num1 = int(input('Enter the First Number : '))
+num2 = int(input('Enter the Second Number : '))
+if num1 > num2:
+    print('Larger NUmber = ', num1)
+else:
+    print('Larger NUmber = ', num1)
+
+
